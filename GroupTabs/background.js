69befdc8899c52b03cb3af36a131a10bcd427530
adetlefsen-rms <augustd@codemagi.com>
2016-05-09 16:18:48 -0700

    Added option to automatically group new tabs

--- GroupTabs/background.js
+++ GroupTabs/background.js
@@ -2,6 +2,9 @@
 // Use of this source code is governed by an Apache-style license that can be
 // found in the LICENSE file.
 
+var urlsToGroup = [];
+var alwaysGroup = false;
+
 /**
  * Parses the domain name from the URL of the current tab.
  *
@@ -9,41 +12,28 @@
  *   is found.
  */
 function getCurrentTabDomain(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
     active: true,
     currentWindow: true
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
     // A window can only have one active tab at a time, so the array consists of
     // exactly one tab.
     var tab = tabs[0];
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    // Get the tab URL
     var url = tab.url;
     
-    //get the domain
+    //get the domain from the URL
     var domain = url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
 
     callback(domain);
   });
 }
   
-chrome.browserAction.onClicked.addListener(function() {
-  getCurrentTabDomain(function(domain) {
-    var urlPattern = "*://" + domain + "/*";
-    groupTabs(urlPattern);
-  });
-});
-
 /**
- * Groups all tabs with URLs matching a pattern
+ * Groups all tabs with URLs matching a pattern into the same window
  */
 function groupTabs(urlPattern) {
   var queryInfo = {
@@ -57,22 +47,80 @@ function groupTabs(urlPattern) {
         chrome.tabs.move(tab.id, {windowId:window.id,index:-1});
       }
       chrome.windows.update(window.id,{focused:true});
+      
+      //remember the URL pattern and teh window it was grouped into
+      urlsToGroup.push({"urlPattern":urlPattern,"window":window.id});
     });
   });  
 }
 
 /**
- * Add a context menu at startup to allow selecting partial URLs to group
+ * Shorthand function to match a wildcard (*) string
  */
-chrome.runtime.onInstalled.addListener(function() {
-  var id = chrome.contextMenus.create({"title": "Specify Tab URLs to Group",
-                                       "contexts":["all"],
-                                       "id": "groupTabsContext"});
+function matchRuleShort(str, rule) {
+  return new RegExp("^" + rule.split("*").join(".*") + "$").test(str);
+}
+
+/**
+ * Click handler action for the main extension button.
+ * 
+ * Causes all tabs from the current domain to be grouped into one window
+ */
+chrome.browserAction.onClicked.addListener(function() {
+  getCurrentTabDomain(function(domain) {
+    var urlPattern = "*://" + domain + "/*";
+    groupTabs(urlPattern);
+  });
 });
 
+/**
+ * Add a listener for new tab events
+ */
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  console.log("urlsToGroup: " + JSON.stringify(urlsToGroup));
+  if (alwaysGroup) {
+    console.log("iterating rules...");
+    for (var i = 0; i < urlsToGroup.length; i++) {
+      var rule = urlsToGroup[i];
+      console.log(JSON.stringify(rule));
+      console.log("trying to match: " + changeInfo.url + " to: " + rule.urlPattern);
+      if (matchRuleShort(changeInfo.url, rule.urlPattern)) {
+        //the new tab URL matches an existing group.
+        //open the new tab in the group window
+        alert("match: " + rule.urlPattern);
+        chrome.tabs.move(tab.id, {windowId:rule.window,index:-1});
+        
+        //focus the newly created tab
+        chrome.windows.update(rule.window,{focused:true});
+        chrome.tabs.update(tab.id, {selected:true});
+      }
+    }
+  }
+});
+
+/**
+ * Add context menus at startup:
+ * - Allow selecting partial URLs with wildcard
+ * - Checkbox to determine whether to always group new tabs that match
+ */
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({"title": "Specify Tab URLs to Group",
+                              "contexts":["all"],
+                              "id": "groupTabsContext"});
+  chrome.contextMenus.create({"title": "Always Group New Tabs",
+                              "contexts":["all"],
+                              "id": "groupTabsAlways",
+                              "type": "checkbox"});
+});
+
+/**
+ * Click handler for context menu items.
+ */
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId === "groupTabsContext") { 
-        groupTabsContextOnClick(info, tab)
+      groupTabsContextOnClick(info, tab)
+    } else if (info.menuItemId === "groupTabsAlways") {
+      groupTabsAlwaysOnClick(info,tab);
     }
 });
 
@@ -80,10 +128,14 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
  * Callback function activated when the context menu item is clicked
  */ 
 function groupTabsContextOnClick(info, tab) {
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
-  
   var regex = window.prompt('Enter URL regex to group (Use * for wildcard)', info.pageUrl);
   
   groupTabs(regex);  
+}
+
+/**
+ * Callback function activated when the context menu item is clicked
+ */ 
+function groupTabsAlwaysOnClick(info, tab) {
+  alwaysGroup = info.checked;
 }
