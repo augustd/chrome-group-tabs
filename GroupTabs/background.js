9457c5be4bccbf1f7ee947416be43dd54cfebfd1
adetlefsen-rms <augustd@codemagi.com>
2016-05-09 10:37:09 -0700

    Add option to specify a URL pattern for grouping
    Right click context menu and enter regex in popup box.

--- GroupTabs/background.js
+++ GroupTabs/background.js
@@ -3,12 +3,12 @@
 // found in the LICENSE file.
 
 /**
- * Get the current URL.
+ * Parses the domain name from the URL of the current tab.
  *
- * @param {function(string)} callback - called when the URL of the current tab
+ * @param {function(string)} callback - called when the domain of the current tab
  *   is found.
  */
-function getCurrentTabUrl(callback) {
+function getCurrentTabDomain(callback) {
   // Query filter to be passed to chrome.tabs.query - see
   // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
@@ -36,21 +36,54 @@ function getCurrentTabUrl(callback) {
 }
   
 chrome.browserAction.onClicked.addListener(function() {
-  getCurrentTabUrl(function(domain) {
-    
+  getCurrentTabDomain(function(domain) {
     var urlPattern = "*://" + domain + "/*";
-    var queryInfo = {
-      url: urlPattern
-    };
-
-    chrome.tabs.query(queryInfo, function(tabs) {
-      chrome.windows.create({tabId:tabs[0].id}, function(window){
-        for (var i = 1; i < tabs.length; i++) {
-          var tab = tabs[i];
-          chrome.tabs.move(tab.id, {windowId:window.id,index:-1});
-        }
-        chrome.windows.update(window.id,{focused:true});
-      });
-    });
+    groupTabs(urlPattern);
   });
 });
+
+/**
+ * Groups all tabs with URLs matching a pattern
+ */
+function groupTabs(urlPattern) {
+  var queryInfo = {
+    url: urlPattern
+  };
+  
+  chrome.tabs.query(queryInfo, function(tabs) {
+    chrome.windows.create({tabId:tabs[0].id}, function(window){
+      for (var i = 1; i < tabs.length; i++) {
+        var tab = tabs[i];
+        chrome.tabs.move(tab.id, {windowId:window.id,index:-1});
+      }
+      chrome.windows.update(window.id,{focused:true});
+    });
+  });  
+}
+
+/**
+ * Add a context menu at startup to allow selecting partial URLs to group
+ */
+chrome.runtime.onInstalled.addListener(function() {
+  var id = chrome.contextMenus.create({"title": "Specify Tab URLs to Group",
+                                       "contexts":["all"],
+                                       "id": "groupTabsContext"});
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === "groupTabsContext") { 
+        groupTabsContextOnClick(info, tab)
+    }
+});
+
+/**
+ * Callback function activated when the context menu item is clicked
+ */ 
+function groupTabsContextOnClick(info, tab) {
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+  
+  var regex = window.prompt('Enter URL regex to group (Use * for wildcard)', info.pageUrl);
+  
+  groupTabs(regex);  
+}
