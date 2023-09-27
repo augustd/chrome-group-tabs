da27b5e10af75982d52107e2635190c785743848
August Detlefsen <adetlefsen@flexport.com>
2023-09-27 13:43:10 -0700

    Eliminate need to load full background.js in popup
    Move shared functions to a separate script

--- GroupTabs/background.js
+++ GroupTabs/background.js
@@ -2,21 +2,23 @@
 // Use of this source code is governed by an Apache-style license that can be
 // found in the LICENSE file.
 
+importScripts("local_storage.js");
+
 const removedTabs = new Set();
 const newTabs = new Set();
 
 
-const preloadHTML = async () => {
-  if (!await chrome.offscreen.hasDocument()) {
-    await chrome.offscreen.createDocument({
-      url: "options.html",
-      reasons: [chrome.offscreen.Reason.DISPLAY_MEDIA],
-      justification: "Helps with faster load times of popup"
-    })
-  }
-}
-
-preloadHTML();
+// const preloadHTML = async () => {
+//   if (!await chrome.offscreen.hasDocument()) {
+//     await chrome.offscreen.createDocument({
+//       url: "options.html",
+//       reasons: [chrome.offscreen.Reason.DISPLAY_MEDIA],
+//       justification: "Helps with faster load times of popup"
+//     })
+//   }
+// }
+//
+// preloadHTML();
 
 /**
  * Message dispatcher
@@ -540,57 +542,3 @@ async function copyLink(url, tab) {
   });
 }
 
-/**
- * Retrieve object from Chrome's Local Storage Area
- * @param {string} key
- */
-const getObjectFromLocalStorage = async function(key) {
-  return new Promise((resolve, reject) => {
-    try {
-      chrome.storage.local.get(key, function(value) {
-        console.time("getObjectFromLocalStorage: " + key);
-        console.log("getObjectFromLocalStorage: " + key);
-        console.log(value[key]);
-        console.log("type: " + (typeof value[key]));
-        if (typeof value[key] === "object") {
-          console.log(value[key]);
-          console.timeEnd("getObjectFromLocalStorage: " + key);
-          resolve(value[key]);
-        } else if (value[key]) {
-          const output = JSON.parse(value[key]);
-          console.log(output);
-          console.timeEnd("getObjectFromLocalStorage: " + key);
-          resolve(output);
-        } else if ("urlsToGroup" === key) {
-          console.timeEnd("getObjectFromLocalStorage: " + key);
-          resolve([]);
-        } else {
-          console.timeEnd("getObjectFromLocalStorage: " + key);
-          resolve({});
-        }
-      });
-    } catch (ex) {
-      reject(ex);
-    }
-  });
-};
-
-/**
- * Save Object in Chrome's Local Storage Area
- * @param {*} obj
- */
-const saveObjectInLocalStorage = async function(key, value) {
-  return new Promise((resolve, reject) => {
-    try {
-      const valueString = JSON.stringify(value);
-
-      console.log("saveObjectInLocalStorage: " + key + "=" + valueString);
-
-      chrome.storage.local.set({[key]:valueString}, function() {
-        resolve();
-      });
-    } catch (ex) {
-      reject(ex);
-    }
-  });
-};
---
+++ GroupTabs/local_storage.js
@@ -0,0 +1,54 @@
+/**
+ * Retrieve object from Chrome's Local Storage Area
+ * @param {string} key
+ */
+const getObjectFromLocalStorage = async function(key) {
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.local.get(key, function(value) {
+                console.time("getObjectFromLocalStorage: " + key);
+                console.log("getObjectFromLocalStorage: " + key);
+                console.log(value[key]);
+                console.log("type: " + (typeof value[key]));
+                if (typeof value[key] === "object") {
+                    console.log(value[key]);
+                    console.timeEnd("getObjectFromLocalStorage: " + key);
+                    resolve(value[key]);
+                } else if (value[key]) {
+                    const output = JSON.parse(value[key]);
+                    console.log(output);
+                    console.timeEnd("getObjectFromLocalStorage: " + key);
+                    resolve(output);
+                } else if ("urlsToGroup" === key) {
+                    console.timeEnd("getObjectFromLocalStorage: " + key);
+                    resolve([]);
+                } else {
+                    console.timeEnd("getObjectFromLocalStorage: " + key);
+                    resolve({});
+                }
+            });
+        } catch (ex) {
+            reject(ex);
+        }
+    });
+};
+
+/**
+ * Save Object in Chrome's Local Storage Area
+ * @param {*} obj
+ */
+const saveObjectInLocalStorage = async function(key, value) {
+    return new Promise((resolve, reject) => {
+        try {
+            const valueString = JSON.stringify(value);
+
+            console.log("saveObjectInLocalStorage: " + key + "=" + valueString);
+
+            chrome.storage.local.set({[key]:valueString}, function() {
+                resolve();
+            });
+        } catch (ex) {
+            reject(ex);
+        }
+    });
+};
